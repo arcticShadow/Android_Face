d5692641af60d3632520a3ab7d884362ce837808
Samuel Attard <samuel.r.attard@gmail.com>
2016-01-26 20:34:19 +1300

    Reworked angle calculations

--- app/src/main/java/com/iceddev/eyesocket/Server.java
+++ app/src/main/java/com/iceddev/eyesocket/Server.java
@@ -1,89 +1,84 @@
 package com.iceddev.eyesocket;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.util.Log;
 
-import com.koushikdutta.async.ByteBufferList;
-import com.koushikdutta.async.DataEmitter;
+import com.iceddev.face.MainActivity;
 import com.koushikdutta.async.callback.CompletedCallback;
-import com.koushikdutta.async.callback.DataCallback;
 import com.koushikdutta.async.http.WebSocket;
 import com.koushikdutta.async.http.libcore.RequestHeaders;
 import com.koushikdutta.async.http.server.AsyncHttpServer;
-import com.koushikdutta.async.stream.ByteBufferListInputStream;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.koushikdutta.async.http.server.AsyncHttpServer.*;
+import static com.koushikdutta.async.http.server.AsyncHttpServer.WebSocketRequestCallback;
 
 /**
  * Created by phated on 8/6/13.
  */
 public class Server {
 
-  List<WebSocket> sockets;
+    List<WebSocket> sockets;
 
-  AsyncHttpServer server;
+    AsyncHttpServer server;
 
-  public Server(int port){
-    sockets = new ArrayList<WebSocket>();
+    private Object lastData;
 
-    server = new AsyncHttpServer();
-    server.websocket("/", new WebSocketRequestCallback() {
-      @Override
-      public void onConnected(final WebSocket webSocket, RequestHeaders requestHeaders) {
-        Log.e("SERVER", "Connected " + webSocket.toString());
-        webSocket.send("Connected");
+    public Server(int port, final MainActivity controller){
+        sockets = new ArrayList<WebSocket>();
 
-        webSocket.setStringCallback(new WebSocket.StringCallback() {
-          @Override
-          public void onStringAvailable(String s) {
-            Log.e("SERVER", "StringAvailable " + webSocket.toString());
-            webSocket.send("echo: " + s);
-          }
+        server = new AsyncHttpServer();
+        server.websocket("/", new WebSocketRequestCallback() {
+            @Override
+            public void onConnected(final WebSocket webSocket, RequestHeaders requestHeaders) {
+                Log.e("SERVER", "Connected " + webSocket.toString());
+                // On initial connect, send the last bit of data
+                if (lastData != null) {
+                    webSocket.send(lastData.toString());
+                }
+
+                webSocket.setStringCallback(new WebSocket.StringCallback() {
+                    @Override
+                    public void onStringAvailable(String imageString) {
+                        String imageDataBytes = imageString.substring(imageString.indexOf(",") + 1);
+                        Log.i("Image", imageDataBytes);
+                        byte[] decodedString = Base64.decode(imageDataBytes.getBytes(), Base64.DEFAULT);
+                        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                        controller.updateViewerPNG(decodedByte);
+                    }
+                });
+
+                webSocket.setClosedCallback(new CompletedCallback() {
+                    @Override
+                    public void onCompleted(Exception e) {
+                        Log.e("SERVER", "Disconnected " + webSocket.toString());
+                        sockets.remove(webSocket);
+                    }
+                });
+
+                sockets.add(webSocket);
+            }
         });
-
-//        webSocket.setDataCallback(new DataCallback() {
-//          @Override
-//          public void onDataAvailable(DataEmitter dataEmitter, ByteBufferList byteBufferList) {
-//            ByteBufferListInputStream stream = new ByteBufferListInputStream(byteBufferList);
-//            try {
-//              Log.e("DATA", );
-//            } catch (IOException e) {
-//              e.printStackTrace();
-//            }
-//          }
-//        });
-
-        webSocket.setClosedCallback(new CompletedCallback() {
-          @Override
-          public void onCompleted(Exception e) {
-            Log.e("SERVER", "Disconnected " + webSocket.toString());
-            sockets.remove(webSocket);
-          }
-        });
-
-        sockets.add(webSocket);
-      }
-    });
-    server.listen(port);
-  }
-
-  public void sendData(Object data) {
-    for (WebSocket socket : sockets) {
-      socket.send(data.toString());
+        server.listen(port);
     }
-  }
 
-  public void close() {
-    for (WebSocket socket : sockets){
-      socket.close();
+    public void sendData(Object data) {
+        lastData = data;
+        for (WebSocket socket : sockets) {
+            socket.send(data.toString());
+        }
     }
-    server.stop();
-  }
+
+    public void close() {
+        for (WebSocket socket : sockets){
+            socket.close();
+        }
+        server.stop();
+    }
 
 }
--- app/src/main/java/com/iceddev/face/MainActivity.java
+++ app/src/main/java/com/iceddev/face/MainActivity.java
@@ -1,53 +1,32 @@
 package com.iceddev.face;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.SensorManager;
-import android.location.LocationManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.text.format.Formatter;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.iceddev.eyesocket.Server;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.io.InputStream;
 
-import static android.text.format.Formatter.*;
-import static android.view.GestureDetector.OnGestureListener;
-import static android.view.GestureDetector.SimpleOnGestureListener;
+import static android.text.format.Formatter.formatIpAddress;
 
-public class MainActivity extends Activity {
-
-  private GestureDetector gestureDetector;
+public class MainActivity extends Activity implements Orientation.Listener {
 
   private static final int PORT = 1338;
   private Server server;
-
-  //allow remote taking of pictures from a broadcast?
-  private static final boolean ALLOW_REMOTE_CAMERA_SHOT =  true;
 
   private static final String LOG_TAG = "SensorTest";
 
@@ -55,58 +34,11 @@ public class MainActivity extends Activity {
 
   private TextView chanText;
 
-  private SensorManager mSensorManager;
+  private ImageView viewer;
 
-  private Sensor mOrientation;
+  private Orientation mOrientation;
 
-  private LocationManager mLocationManager;
-
-  float azimuth_angle;
-  float pitch_angle;
-  float roll_angle;
-  String lastMessage = "";
-  Object lastError = null;
-
-  private Data sensorData;
-
-  private Map<String, Object> data;
-
-  public void setButtons(String keyPressed){
-    JSONObject buttons = (JSONObject) data.get("buttons");
-    Iterator keys = buttons.keys();
-    while(keys.hasNext()){
-      String key = (String) keys.next();
-      try {
-        buttons.put(key, key.equals(keyPressed));
-      } catch (JSONException jse){
-        Log.w(LOG_TAG, "error serializing json" + jse.toString());
-      }
-    }
-  }
-
-  public void onClick_A(View view) {
-    sensorData.setButton("A", true);
-  }
-
-  public void onClick_B(View view) {
-    sensorData.setButton("B", true);
-  }
-
-
-  public void broadcastWebsocket(Map<String, Object> map){
-    try{
-      JSONObject jsObj = new JSONObject();
-      for (String key : map.keySet()) {
-        jsObj.put(key, map.get(key));
-      }
-      JSONArray arguments = new JSONArray();
-      arguments.put(jsObj);
-      server.sendData(arguments);
-    }catch(Exception jse){
-      server.sendData(jse);
-      Log.w(LOG_TAG, "error broadcasting" + jse.toString());
-    }
-  }
+  private String lastSent;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -119,37 +51,16 @@ public class MainActivity extends Activity {
 
     text = (TextView) findViewById(R.id.text);
     chanText = (TextView) findViewById(R.id.chan);
+    viewer = (ImageView) findViewById((R.id.imageView));
     WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
     WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
     int ip = wifiInfo.getIpAddress();
     final String ipAddress = formatIpAddress(ip);
-    chanText.setText("IP: " + ipAddress + ":"+this.PORT);
+    chanText.setText("IP: " + ipAddress + ":"+ this.PORT);
 
-    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-    for(Sensor sensor : sensorList) {
-      Log.i(LOG_TAG, "Found sensor: " + sensor.getName());
-    }
-
-    gestureDetector = new GestureDetector(this, gestureListener);
-
-    mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-
-    data = new HashMap<String, Object>();
-
-    JSONObject gestures = new JSONObject();
-    data.put("gestures", gestures);
-    JSONObject sensors = new JSONObject();
-    data.put("sensors", sensors);
-    JSONObject buttons = new JSONObject();
-    try{
-      buttons.put("A", false);
-      buttons.put("B", false);
-    } catch (JSONException jse){
-      Log.w(LOG_TAG, "error serializing json" + jse.toString());
-    }
-
-    data.put("buttons", buttons);
+    lastSent = "";
+    mOrientation = new Orientation((SensorManager) getSystemService(Activity.SENSOR_SERVICE),
+            getWindow().getWindowManager());
   }
 
   @Override
@@ -161,137 +72,44 @@ public class MainActivity extends Activity {
   @Override
   protected void onResume() {
     super.onResume();
-    server = new Server(PORT);
-    sensorData = new Data(server);
-    mSensorManager.registerListener(orientationSensor, mOrientation, SensorManager.SENSOR_DELAY_UI);
-    mSensorManager.registerListener(initialOrientationSensor, mOrientation, SensorManager.SENSOR_DELAY_UI);
+    server = new Server(PORT, this);
+    mOrientation.startListening(this);
   }
 
   @Override
   protected void onPause() {
     super.onPause();
     server.close();
-    mSensorManager.unregisterListener(orientationSensor);
+    mOrientation.stopListening();
   }
 
   @Override
-  public boolean onGenericMotionEvent(MotionEvent event) {
-    gestureDetector.onTouchEvent(event);
-    return false;
-  }
-
-  protected String displayString(Object obj){
-    if(obj == null){
-      return "";
-    }else{
-      String longStr = obj.toString();
-      if(longStr.length() > 15){
-        return longStr.substring(0,14);
-      }else{
-        return longStr;
-      }
+  public void onOrientationChanged(final float pitch, final float roll) {
+    final JSONObject data = new JSONObject();
+    try {
+      data.put("pitch", Float.valueOf(pitch).intValue());
+      data.put("roll", Float.valueOf(roll).intValue());
+    } catch (JSONException e) {
+      Log.i("JSON Error: ", e.getMessage());
+    }
+    if (!data.toString().equals(lastSent)) {
+      lastSent = data.toString();
+      server.sendData(data);
     }
   }
 
-  private SensorEventListener initialOrientationSensor = new SensorEventListener() {
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-      azimuth_angle = sensorEvent.values[0];
-      pitch_angle = sensorEvent.values[1];
-      roll_angle = sensorEvent.values[2];
-
-      HashMap<String, Float> orientation = new HashMap<String, Float>();
-
-      orientation.put("pitch", pitch_angle);
-      orientation.put("azimuth", azimuth_angle);
-      orientation.put("roll", roll_angle);
-      sensorData.setInitialOrientation(orientation);
-
-      mSensorManager.unregisterListener(initialOrientationSensor);
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-  };
-
-  private SensorEventListener orientationSensor = new SensorEventListener() {
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-      azimuth_angle = sensorEvent.values[0];
-      pitch_angle = sensorEvent.values[1];
-      roll_angle = sensorEvent.values[2];
-
-      runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          text.setText("msg:" + displayString(lastMessage) +
-                  "\n" + "a,p,r:" + new Float(azimuth_angle).intValue() + "," +  new Float(pitch_angle).intValue() + "," + new Float(roll_angle).intValue() +
-                  "\n" + "err:" + displayString(lastError));
+  public void updateViewerPNG(final Bitmap image) {
+    runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          if (image != null) {
+            viewer.setImageBitmap(image);
+          }
+        } catch (Exception e) {
+          Log.i("Bitmap Error", e.getMessage());
         }
-      });
-
-      HashMap<String, Float> orientation = new HashMap<String, Float>();
-
-      orientation.put("pitch", pitch_angle);
-      orientation.put("azimuth", azimuth_angle);
-      orientation.put("roll", roll_angle);
-      sensorData.setOrientation(orientation);
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-  };
-
-  private OnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
-
-    @Override
-    public boolean onDown(MotionEvent motionEvent) {
-      sensorData.setGesture("down", true);
-      return false;
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent motionEvent) {
-      sensorData.setGesture("doubleTap", true);
-      return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent motionEvent) {
-//       sensorData.setGesture("longPress", true);
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent motionEvent) {
-      sensorData.setGesture("tap", true);
-      return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent2, float distanceX, float distanceY) {
-      Log.d("Gesture Example", "onScroll: distanceX:" + distanceX + " distanceY:" + distanceY);
-      if (distanceX < -1) {
-        sensorData.setGesture("scrollRight", true);
-      } else if (distanceX > 1) {
-        sensorData.setGesture("scrollLeft", true);
       }
-      return false;
-    }
-
-    public void onLongPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent2, float v, float v2) {
-      return false;
-    }
-  };
-
+    });
+  }
 }
